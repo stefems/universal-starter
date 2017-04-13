aa5ebcf196ab030c9edb992598855cb6710ac494
Stefan Kowal <kowalmax.s@gmail.com>
2017-04-12 22:28:49 -0600

    testing angular2 dynamic styling

--- src/+app/facebooklogin.component.ts
+++ src/+app/facebooklogin.component.ts
@@ -5,17 +5,23 @@ import {FacebookService, FacebookInitParams, FacebookLoginResponse} from 'ng2-fa
   selector: 'facebooklogin',
   template: `
   <div class="loginDiv">
-    <p>Log In via {{text}}</p>
-    <input (click)="checkLogin()" type="submit" value="Submit">
+    <p>Log In via {{text}}. Status: {{logStatus}}</p>
+    <input [ngClass]="{invisible: hasInvisibleClass}" (click)="checkLogin()" type="submit" value="Submit">
   </div>
   `,
-  styles: [``],
+  styles: [`
+  .invisible {
+    display: none;
+  }
+  `],
   providers: [FacebookService]
 })
 
 export class FacebookLoginComponent {
 
 	text = 'Facebook';
+  logStatus = 'Not logged in.';
+  hasInvisibleClass = false;
 
 	constructor(private fb: FacebookService) {
 		let fbParams: FacebookInitParams = {
@@ -28,13 +34,17 @@ export class FacebookLoginComponent {
  	
   checkLogin(): void {
     this.fb.login().then(
-      (response: FacebookLoginResponse) => test(), (error: any) => other()
+      (response: FacebookLoginResponse) => success(), (error: any) => failure()
     );
-    function test() {
+    function success() {
+      this.logStatus = "logged in as " + "";
+      this.hasInvisibleClass = true;
       console.log("Login successful.")
+      //remove this component and add the default calendar one
     }
-    function other() {
+    function failure() {
       console.error("facebook login eror")
+      //put text on page showing error
     }
   }
 }
