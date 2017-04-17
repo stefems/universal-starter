c97f49691c1f96a91de7b7015e60519afbe855ed
Stefan Kowal <kowalmax.s@gmail.com>
2017-04-17 14:27:22 -0600

    fixed component html

--- src/+app/events.component.ts
+++ src/+app/events.component.ts
@@ -10,8 +10,8 @@ declare var soundCloud:any;
     <input (click)="loadEvents()" type="submit" value="Load Events">
     <h1>Player:</h1>
     <object height="81" width="100%" id="yourPlayerId" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
-	  <param name="movie" value="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fsoundcloud.com%2Fmatas%2Fhobnotropic&enable_api=true&object_id=yourPlayerId"></param>
-	  <param name="allowscriptaccess" value="always"></param>
+	  <param name="movie" value="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fsoundcloud.com%2Fmatas%2Fhobnotropic&enable_api=true&object_id=yourPlayerId">
+	  <param name="allowscriptaccess" value="always">
 	  <embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fsoundcloud.com%2Fmatas%2Fhobnotropic&enable_api=true&object_id=yourPlayerId" type="application/x-shockwave-flash" width="100%" name="yourPlayerId"></embed>
 	</object>
   `
